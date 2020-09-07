75157575932c102ec1523423fa2378e2f21a9a96
lyn-liu210 <lyn.cunlong.liu@accenture.com>
2020-09-07 17:14:23 +0800

    1

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,15 @@
 
 import express=require('express');
-// import {  dbtest} from "./routes/dbtest";
 import "reflect-metadata";
-
-var user = require('./routes/userroute');
+const user = require('./routes/userroute');
+const bodyParser = require('body-parser');
 const app:express.Application=express();
+app.use( bodyParser.urlencoded({extended: false}) )
+app.use(bodyParser.json({ limit: '50mb' }))
 app.get('/',function(req,res){
     res.send('Hello World!');
 });
-// host: string,
-// port: number,
-// username: string,
-// password: string,
-// database: string
-console.log("__dirname", __dirname)
 app.use("/users",user)
-app.listen(8888,function(){
-    console.log('Example app listening on port 8888!');
+app.listen(8080,function(){
+    console.log('Example app listening on port 8080!');
 })
-
-// createConnection({
-//     type: "mssql",
-//     host: "DLC000G4C59H2LD",
-//     // domain: "DIR",
-//     username:"sa",
-//     password:"123456!Q@W",
-//     port: 1433,
-//     database: "innovationhub",
-//     extra: {
-//         driver: sqlDriver,
-//         options: {
-//             "trustedConnection": true
-//         },
-//     },
-//     entities: [
-//         __dirname + "/entity/*.js"
-//     ],
-//     synchronize: true,
-//     logging: false
-// }).then(connection => {
-//     console.log("connection successfully!")
-// }).catch(error => console.log(error));
--- src/routes/userroute.ts
+++ src/routes/userroute.ts
@@ -1,25 +1,21 @@
 var express = require('express');
 var router = express.Router();
-import {DatabaseService}  from "../service/dbservice"
+import { DatabaseService } from "../service/dbservice"
 import { DBHelper } from '../utils/dbhelper';
 import { User } from '../entity/user';
-// const myPool = require('./db_pool');
-const sql = require("mssql/msnodesqlv8");
-/* GET users listing. */
 router.post('/api', (req, res) => {
-  const database : DatabaseService = new DatabaseService(new DBHelper("DLC000G4C59H2LD",1433,"sa","123456!Q@W","innovationhub",));
-  const user : User = new User()
-  user.name = "Lyn"
-  user.gender="M"
-  user.age = 12
+  const database: DatabaseService = new DatabaseService(new DBHelper("DLC000G4C59H2LD", 1433, "sa", "123456!Q@W", "innovationhub",));
+  const body = req.body
+  const user: User = new User()
+  user.name = body.name
+  user.gender = body.gender
+  user.age = body.age
   database.createOrUpdateEntity<User>(user).then((data) => {
-    if(data){
+    if (data) {
       res.send(data)
     } else {
       res.send("error")
     }
   });
-  // rs.
-  });
-
+});
 module.exports = router;
